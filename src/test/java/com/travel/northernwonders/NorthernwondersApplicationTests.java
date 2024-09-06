e291cfd52d3bb69624ddda8f917d6e004c1debf5
Leticia <leticiaborchardt123@gmail.com>
2024-09-06 20:13:19 -0300

    initial commit

---
+++ src/test/java/com/travel/northernwonders/NorthernwondersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travel.northernwonders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NorthernwondersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
