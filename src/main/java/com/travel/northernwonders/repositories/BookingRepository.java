ebfd81aea519a5d69b44a837328cf05d756752b0
Leticia <leticiaborchardt123@gmail.com>
2024-09-18 20:13:00 -0300

    feat: remove the booking entity

--- src/main/java/com/travel/northernwonders/repositories/BookingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.travel.northernwonders.repositories;
-
-import com.travel.northernwonders.domain.booking.Booking;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookingRepository extends JpaRepository<Booking, String> {
-}
